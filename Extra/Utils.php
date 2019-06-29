a705a79c1ea96c06149865736b238b48961106d7
shine.yi <806107116@qq.com>
2019-06-29 19:07:15 +0800

    第二版

--- Extra/Utils.php
+++ Extra/Utils.php
@@ -4,12 +4,43 @@ namespace Keylink\Extra;
 
 class Utils {
 	//去重关键词
-	public static function mergeUniqueResult($result = []) {
-		if (empty($result)) {
+	public static function mergeUniqueResult($data = [], $existCount = 1) {
+		$result = $strLenMap = $posMap = [];
+		if (empty($data)) {
+			return $result;
+		}
+		foreach ($data as $row) {
+			list($pos, $str) = $row;
+			$strLen = mb_strlen($str);
+			isset($posMap[$pos]) || $posMap[$pos] = [];
+			$posMap[$pos][$strLen] = $str;
+		}
+		$posArr = array_keys($posMap);
+		$nextMinPos = $i = 0;
+		$lastStr = '';
+		while($i < count($posArr)) {
+			$tmPos = $posArr[$i];
+			$i++;
+			$posRow = $posMap[$tmPos];
+			krsort($posRow);
+			$tmPosRow = self::array_key_value_shift($posRow);
+			if ($tmPos < $nextMinPos && strpos($lastStr, $tmPosRow['str']) !== false) {
+				continue;
+			}
+			$result[$tmPos] = $tmPosRow;
+			$nextMinPos = $tmPos + $tmPosRow['len'];
+			$lastStr = $tmPosRow['str'];
+		}
+		return $result;
+	}
+
+	public static function array_key_value_shift($arr) {
+		if (empty($arr)) {
 			return [];
 		}
-		foreach ($result as $row) {
-			
-		}
+		return [
+			'len' => array_keys($arr)[0],
+			'str' => array_values($arr)[0]
+		];
 	}
 }
--- index.php
+++ index.php
@@ -8,10 +8,52 @@ include 'Extra/Utils.php';
 
 $keywords = new Keyword(['中华', 'he', 'she', 'his', 'hers']);
 
-$result = $keywords->searchIn( 'She 在苏打水打算的爱=箭hishers迪生中she华的中撒所大撒的' );
+$result = $keywords->searchIn( 'She 在苏打水打算的爱=箭hishers迪生中she华的中撒he所大撒的' );
 
 print_r($result);
 
 $result = Utils::mergeUniqueResult($result);
 
 print_r($result);
+
+
+/**
+
+[0] => Array
+        (
+            [0] => 1
+            [1] => he
+        )
+
+    [1] => Array
+        (
+            [0] => 14
+            [1] => his
+        )
+
+    [2] => Array
+        (
+            [0] => 16
+            [1] => she
+        )
+
+    [4] => Array
+        (
+            [0] => 17
+            [1] => hers
+        )
+
+    [5] => Array
+        (
+            [0] => 24
+            [1] => she
+        )
+
+
+    [7] => Array
+        (
+            [0] => 31
+            [1] => he
+        )
+1 14 16 17 24 31
+*/
